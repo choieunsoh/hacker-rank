b8f3a1b2dc356a8e8bc896ba835600626c19960f
Kiattipong Kamonrat <kiattipong@thaibma.or.th>
2022-03-20 08:48:52 +0700

    Add GridChallenge solution

--- 01.1_Week_Preparation_Kit/Day_04/01E_GridChallenge.js
+++ 01.1_Week_Preparation_Kit/Day_04/01E_GridChallenge.js
@@ -1,15 +1,14 @@
 function gridChallenge(grid) {
   // Write your code here
+  const lines = []
   for (let i = 0; i < grid.length; i++) {
-    grid[i] = grid[i]
-      .split('')
-      .sort((a, b) => a.localeCompare(b))
-      .join('')
+    const line = grid[i].split('').sort()
+    lines.push(line)
   }
 
-  for (let c = 0; c < grid[0].length; c++) {
-    for (let r = 0; r < grid.length - 1; r++) {
-      if (grid[r][c].localeCompare(grid[r + 1][c]) !== -1) {
+  for (let c = 0; c < lines[0].length; c++) {
+    for (let r = 1; r < lines.length; r++) {
+      if (lines[r - 1][c] > lines[r][c]) {
         return 'NO'
       }
     }
